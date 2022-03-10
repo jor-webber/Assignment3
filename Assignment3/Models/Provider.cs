62d4921b0eb8ee74cdb6e9e5a165aafddbe1460c
jordan webber <webber_jordan_95@hotmail.com>
2022-03-10 12:46:29 -0500

    Added Provider model class.

---
+++ Assignment3/Models/Provider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Provider
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string LastName { get; set; }
+        [Required]
+        public uint LicenseNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
