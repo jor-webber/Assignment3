6fbc055691b366a73d2111055640442f0fff3e5f
Harmanpreet Dhillon <harmansdhillon97@gmail.com>
2022-03-10 12:57:53 -0500

    patient class

---
+++ Assignment3/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace assignment3.Models
+{
+    public class Patient
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTimeOffset DateOfBirth { get; set; }
+    }
+}
