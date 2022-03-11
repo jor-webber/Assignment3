27ce02bdb0fc3cb5653119025c59d677ca7e9ce3
Ramandeep Ghotra <raman45@icloud.com>
2022-03-11 09:43:40 -0500

    Organziation and provider classes

---
+++ Assignment3/Models/Organization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace W7D2_webAPI.Models
+{
+    public class Organization
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("Hospital|Clinic|Pharmacy", ErrorMessage = "Only Hospital, Clinic, Pharmacy allowed.")]
+        public string Type { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
