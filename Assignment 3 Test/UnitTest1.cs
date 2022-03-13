523b25ec10925c047213b32cf662176d5e8bd0e7
Ramandeep Ghotra <raman45@icloud.com>
2022-03-13 14:12:26 -0400

    created test class

---
+++ Assignment 3 Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Assignment3.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment_3_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetAllPatients()
+        {
+            // Arrange
+            var controller = new PatientController();
+        }
+    }
+}
