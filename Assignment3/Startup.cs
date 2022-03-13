ede2da27d96f5a916f3f3bc72395442c8c627bfb
jordan webber <webber_jordan_95@hotmail.com>
2022-03-13 13:48:23 -0400

    Add Xml and Json verification

--- Assignment3/Startup.cs
+++ Assignment3/Startup.cs
@@ -27,7 +27,7 @@ namespace Assignment3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddXmlDataContractSerializerFormatters();
 
             services.AddDbContext<Assignment3Context>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("Assignment3Context")));
